a64fad757cc927fb1632412da032eb89586215b0
yibre <cdoyue@gmail.com>
2023-04-16 19:27:30 +0900

    [Feat] make app named reference, set pw for postgresql

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'drawingreferwiki',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'psql1234',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
